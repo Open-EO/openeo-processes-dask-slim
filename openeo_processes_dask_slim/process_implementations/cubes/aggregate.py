94750bd7040456db6b93a29ec9710cf9d807ac2e
ValentinaHutter <valentinahutter@gmail.com>
2026-06-29 14:35:27 +0200

    aggregate temporal period with numpy datetime64 ns

--- openeo_processes_dask_slim/process_implementations/cubes/aggregate.py
+++ openeo_processes_dask_slim/process_implementations/cubes/aggregate.py
@@ -241,7 +241,9 @@ def aggregate_temporal_period(
     if period in periods_to_frequency.keys():
         frequency = periods_to_frequency[period]
 
-        times_ns = pd.DatetimeIndex(data[applicable_temporal_dimension].values).as_unit("ns")
+        times_ns = pd.DatetimeIndex(data[applicable_temporal_dimension].values).as_unit(
+            "ns"
+        )
 
         data[applicable_temporal_dimension] = times_ns
 
